0d8395f7290d55556e2ce46d19fef65d79e1e97e
woleDiamond <98061586+woleDiamond@users.noreply.github.com>
2022-03-08 19:18:50 -0600

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "mydev-tf-state-bucket"
+    bucket = "my-jan-state-s3-bucket"
     key = "main"
-    region = "us-east-2"
+    region = "us-east-1"
     dynamodb_table = "my-dynamodb-table"
   }
 }
